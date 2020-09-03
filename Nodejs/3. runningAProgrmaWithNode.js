1f18eb7070af33be6f273e364b6ac37babc08923
KOKI <agiks21@gmail.com>
2020-09-03 23:19:00 +0900

    Nodejs_3,4 added

---
+++ Nodejs/3. runningAProgrmaWithNode.js
@@ -0,0 +1,17 @@
+let noun1 = '____';
+let adjective = '____';
+let noun2 = '____';
+let verb = '____';
+let noun3 = '____';
+
+
+console.log(`The world's first ${noun1} was a very ${adjective} ${noun2} who loved to ${verb} while eating ${noun3} for every meal.`);
+
+/*
+We’ve written a silly sentence JavaScript program in the app.js file. There are a number of variables assigned the string '____'. Replace each of them with words of the designated type:
+let adjective = 'silly';
+*/
+
+/*
+Let’s run the program in the terminal so we can see its output. Type node app.js in the terminal and press enter and then press “Check Work”. You should see the output of the program in the terminal!
+*/
